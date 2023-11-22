fe7ace4400583651b9f5f64e5427e87f85baae12
Drag-3 <juserysthee@gmail.com>
2023-11-22 10:21:03 -0500

    Changing links in html to use flask url_for

--- approot/pages.py
+++ approot/pages.py
@@ -6,3 +6,8 @@ webpages = Blueprint('webpages', __name__, template_folder='../webroot/templates
 @webpages.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+@webpages.route('/flowers/', methods=['GET'])
+def flowers():
+    return render_template('flowers.html')
